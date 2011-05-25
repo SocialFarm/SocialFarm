72ff25acd09ce0ccf1d95c830c0d7961ad2f5fe6
Vivek Pathak <vpathak@stevens.edu>
2011-05-25 20:23:15 +0000

    added initial version from steve
    needs to be refactored/renamed

---
+++ socialfarm/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls.defaults import *
+from django.conf import settings
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^fbbuis/', include('fbbuis.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/(.*)', admin.site.root),
+    (r'^fbbuis/', include('fbbuis.fbbuismanage.urls')),
+    (r'^styles/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.STATIC_DOC_ROOT}),
+)
