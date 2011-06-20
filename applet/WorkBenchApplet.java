cd76e64f4c07f5c6272235d6fa559b402711681f
Vivek Pathak <vpathak@stevens.edu>
2011-06-20 19:23:00 +0000

    exploring

---
+++ applet/WorkBenchApplet.java
@@ -0,0 +1,12 @@
+
+// http://dev.eclipse.org/viewsvn/viewvc.cgi/trunk/we/jwt-we/src/org/eclipse/jwt/we/editors/wizards/creation/CreationWizard1.java?view=markup&revision=13&root=SOA_JWT&pathrev=13 
+
+import org.eclipse.jwt.we.editors.WEEditor;
+import org.eclipse.ui.IWorkbench;
+
+class WorkBenchApplet { 
+   
+   protected IWorkbench workbench;
+
+   
+} 
