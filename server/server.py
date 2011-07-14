bd11138f7f139974f6197881eea93bd779fe2e5d
Orie Steele <osteele@stevens.edu>
2011-07-14 23:39:25 +0000

    added canvas view

--- server/server.py
+++ server/server.py
@@ -103,9 +103,70 @@ templates_wrappers = {
 #'deletebusiness' : CodeWrapper('/deletebusiness/{id}', pythonobj , method ) 
 }
 
+def render_canvas(request):
+    request.send_response(200)
+    request.send_header('Content-type', 'text/html')
+    request.end_headers()
+
+    request.wfile.write("""
+<!doctype html>  
+<html lang="en">
+    <head>  
+        <meta charset="utf-8">  
+        <title>social_farm</title>  
+        <meta name="description" content="...">  
+        <meta name="author" content="Social Farm"> 
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js" type="text/javascript"></script> 
+
+        <!--
+        ### in the future prefex script should be included here and called below
+        ### for now I am imitating what it would do below, normally, the document ready would look like: 
+        ### prefix_js.run()
+        ---> 
+        
+        <script>
+ 
+        $(document).ready(function() {
+            alert('canvas view');
+        });
+        </script>
+
+    </head>  
+    <body>
+        <h1>social_farm</h1>
+        <div id="json_wrapper"></div>
+
+        <div id="fb-root"></div>
+        <script>
+            window.fbAsyncInit = function() {
+                FB.init({
+                appId  : "%s",
+                status : true, // check login status
+                cookie : true, // enable cookies to allow the server to access the session
+                xfbml  : true  // parse XFBML
+                });
+            };
+
+            (function() {
+                var e = document.createElement('script');
+                e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+                e.async = true;
+                document.getElementById('fb-root').appendChild(e);
+            }());
+            FB.api('/me', function(response) {
+                alert(response.name);
+            });
+        </script>
+        
+    </body>
+</html>
+""" % FACEBOOK_APP_ID)
+
 
 class BaseHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        if len(self.path.split('/')) == 2:
+            render_canvas(self);
         if self.path.split('/')[1] in templates_wrappers.keys(): 
             templates_wrappers[self.path.split('/')[1]].get(self)
            
