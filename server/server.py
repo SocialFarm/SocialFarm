8174200ec13e4e649a45d5cc08b46ffd5fb6515e
Orie Steele <osteele@stevens.edu>
2011-07-14 20:12:29 +0000

    testing oauth token from cookies

--- server/server.py
+++ server/server.py
@@ -1,57 +1,59 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import facebook
 
-PORT = 80
+PORT = 8080
 FACEBOOK_APP_ID       = '234690403213067'
 FACEBOOK_API_KEY      = '7165704a80616d58e50629512349a8c1'
 FACEBOOK_APi_SECRET   = '14a74c4bcf2537f42d714c5cd8324c6e'
-FACEBOOK_CALLBACK_URL = 'http://apps.facebook.com/social_farm/facebook/'
+FACEBOOK_CALLBACK_URL = 'http://apps.facebook.com/social_farm/'
 
-class BaseHandler(BaseHTTPRequestHandler):
+class FBHandler(BaseHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        self.user = None
+        BaseHTTPRequestHandler.__init__(self, *args, **kwargs)
+
+    def setUser(self):
+        cookies = self.headers.get('Cookie')
+        if cookies:
+            morsel = Cookie.BaseCookie(cookies).get('user')
+            if morsel:
+                self.user = morsel.value
+
     def do_GET(self):
-        if self.path.split("/")[1] in Handlers.keys():
-            Handlers[self.path.split("/")[1]].login(self)
+
+        self.setUser()
+
+        if not self.user:
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            response = """
+            <script language="javascript">
+                top.location.href='https://graph.facebook.com/oauth/authorize?client_id={0}&redirect_uri={1}&display=page&scope=publish_stream';
+            </script>
+            <noscript>
+            <a href="https://graph.facebook.com/oauth/authorize?client_id={0}&redirect_uri={1}&display=page&scope=publish_stream" target="_top">
+                Authorize this application
+            </a>
+            </noscript>""".format(FACEBOOK_APP_ID,FACEBOOK_CALLBACK_URL)
+            self.wfile.write(response)
         else:
-            self.send_error(404,'Resource Not Found: %s' % self.path)
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write("user: %s" % (self.user))
 
     def do_POST(self):
         print self.path.split("/")
 
-class FacebookHandler():
-    def login(self, request):
-        if not False:
-            request.send_response(200)
-            request.send_header('Content-type', 'text/html')
-            request.end_headers()
-            response = """
-            <script language="javascript">
-            top.location.href='https://graph.facebook.com/oauth/authorize?client_id={0}&redirect_uri={1}&display=page&scope=publish_stream';
-            </script>
-            <noscript>
-            <a href="https://graph.facebook.com/oauth/authorize?client_id={0}&redirect_uri={1}&display=page&scope=publish_stream" target="_top">Click here to authorize this application</a>
-            </noscript>""".format(FACEBOOK_APP_ID,FACEBOOK_CALLBACK_URL)
-            request.wfile.write(response)
-        else:
-            request.send_response(301)
-            request.send_header("Location", FACEBOOK_CALLBACK_URL)
-            request.end_headers()
 
-    def get(self, request):
-        request.send_response(200)
-        request.send_header('Content-type', 'text/html')
-        request.end_headers()
-        request.wfile.write("foo")
 
-    def post(self, request):
-        print "post"
-
-Handlers = {'facebook': FacebookHandler()}
 
 
 def main():
     try:
         server_address = ('', PORT)
-        server = HTTPServer(server_address, BaseHandler)
+        server = HTTPServer(server_address, FBHandler)
         print 'started httpserver...'
         server.serve_forever()
     except KeyboardInterrupt:
