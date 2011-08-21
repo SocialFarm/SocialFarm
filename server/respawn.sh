92ebe80ac4264158b89dbdab47c82a51244bf253
Orie Steele <osteele@stevens.edu>
2011-08-21 18:25:05 +0000

    testing wfe

--- server/respawn.sh
+++ server/respawn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-until server; do
-    echo "server crashed. exit code: $?. Respawning..." > &2
+until python server.py; do
+    echo "server crashed. exit code: $?. Respawning..." >&2
     sleep 1
 done
