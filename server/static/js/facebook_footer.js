44da58bcce1dd6e7c5e679bbcd849a4283553914
Vivek Pathak <vpathak@stevens.edu>
2011-08-22 20:55:56 +0000

    added event and subscribe code at top to be tested

--- server/static/js/facebook_footer.js
+++ server/static/js/facebook_footer.js
@@ -1,43 +1,113 @@
 //facebook_footer.js 
 //facebook js code for socialfarm.org
 var user = null;
+var fbresponse = null; 
+
+
+function LOG(mesg) { 
+    console.log(mesg) ; 
+}
+    
+
+(function() {
+    var e = document.createElement('script');
+    e.type = 'text/javascript';
+    e.src = document.location.protocol +
+        '//connect.facebook.net/en_US/all.js';
+    e.async = true;
+    document.getElementById('fb-root').appendChild(e);
+}());
+
+
 
 window.fbAsyncInit = function() {
 	FB.init({ appId: '234690403213067', 
 		status: true, 
 		cookie: true,
-		xfbml: true,
+		xfbml: false,
 		oauth: true});
 
-   function updateButton(response) {
-		
+	/* Get permission for the app */ 
+	FB.login(function(response) {
 		if (response.authResponse) {
-			
-		    //user is already logged in and connected
-		    FB.api('/me', function(info) {
-		        login(response, info);
-		    });
-
-            $('button#login').text('Logout');
-            $('button#login').click(function() {
-                FB.logout(function(response) { logout(response); });
-            });  
-		    
+		    console.log('Welcome!  Fetching your information.... ');
+		    FB.api('/me', function(response) {
+			    console.log('Good to see you, ' + response.name + '.');
+			    FB.logout(function(response) {
+				    console.log('Logged out.');
+				});
+			});
 		} else {
-		    //user is not connected to your app or logged out
-		    $('button#login').text('Login');
-		    $('button#login').click(function() {
-                login_prompt();
-            });
-		        
+		    console.log('User cancelled login or did not fully authorize.');
 		}
-	}
+	    }, {scope: 'email'});
+
+
+	/* On login, make the button say logout */
+	FB.Event.subscribe('auth.login', function(response) {
+		LOG( "Got auth.login" ) ; 
+		$('button#login').text('Logout');
+		$('button#login').click(function() {
+			FB.logout(function(response) { logout(response); });
+		    });
+		login() ; 
+	    });
+
+	/* opposite of above */ 
+	FB.Event.subscribe('auth.logout', function(response) {
+		LOG( "Got auth.logout" ) ;
+		$('button#login').text('Login');
+		$('button#login').click(function() {
+			login_prompt();
+		    });
+	    });
+
+	LOG( "Calling get login status" ) ; 
+	FB.getLoginStatus(function(response) {
+		alert("Hi in get loginstatus");
+		fbresponse = response; 
+		if (response.session) {
+		    // logged in and connected user, someone you know
+		    login();
+		}
+	    });
+	LOG( "FB response is " + fbresponse ) ; 
+
+
+
+   function updateButton(response) {
+       alert("Hi there");
+       LOG( "Inside update button" ) ; 
+       if (response.authResponse) {
+	   fbresponse = response ; 
+	   //user is already logged in and connected
+	   FB.api('/me', function(info) {
+		   login(response, info);
+	       });
+
+	   $('button#login').text('Logout');
+	   $('button#login').click(function() {
+		   FB.logout(function(response) { logout(response); });
+	       });  
+		    
+       } else {
+	   LOG('user not connected, and about to assign call back on click'); 
+	   //user is not connected to your app or logged out
+	   $('button#login').text('Login');
+	   $('button#login').click(function() {
+		   login_prompt();
+	       });
+		        
+       }
+   }
+
 
     function login_prompt(){
         FB.login(function(response) {
             if (response.authResponse) {
                 FB.api('/me', function(info) {
                     login(response, info);
+		    fbresponse = response; 
                 });    
             } else {
                 //user cancelled login or did not grant authorization
@@ -47,8 +117,10 @@ window.fbAsyncInit = function() {
     }
 
 	// run once with current status and whenever the status changes
-	FB.getLoginStatus(updateButton);
-	FB.Event.subscribe('auth.statusChange', updateButton);	
+	//LOG( "about to call get login status" ) ; 
+	//FB.getLoginStatus(updateButton);
+	//LOG( "FB Response is " + fbresponse ) ; 
+	//FB.Event.subscribe('auth.statusChange', updateButton);	
 };
 (function() {
 	var e = document.createElement('script'); e.async = true;
