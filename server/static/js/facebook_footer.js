b0fcfa14388a5bd170ca54159d0b17d4dd4f7c7d
Orie Steele <osteele@stevens.edu>
2011-08-23 22:28:06 +0000

    testing cut and past FB code

--- server/static/js/facebook_footer.js
+++ server/static/js/facebook_footer.js
@@ -63,40 +63,33 @@ window.fbAsyncInit = function() {
 		oauth: true});
     
     function updateButton(response) {
-    var button = document.getElementById('login');
+
+    var button = $('#login');
 		
     if (response.authResponse) {
       //user is already logged in and connected
-      var userInfo = document.getElementById('user-info');
+      var userInfo = $('.user #user-info');
       FB.api('/me', function(response) {
-        userInfo.innerHTML = '<img src="https://graph.facebook.com/' 
-	  + response.id + '/picture">' + response.name;
-        button.innerHTML = 'Logout';
+        FBOnLoad();
+        
       });
-      button.onclick = function() {
-        FB.logout(function(response) {
-          var userInfo = document.getElementById('user-info');
-          userInfo.innerHTML="";
-	});
-      };
+        set_logout_button();
+     
     } else {
-      //user is not connected to your app or logged out
-      button.innerHTML = 'Login';
-      button.onclick = function() {
-        FB.login(function(response) {
-	  if (response.authResponse) {
-            FB.api('/me', function(response) {
-	      var userInfo = document.getElementById('user-info');
-	      userInfo.innerHTML = 
-                '<img src="https://graph.facebook.com/' 
-	        + response.id + '/picture" style="margin-right:5px"/>' 
-	        + response.name;
-	    });	   
-          } else {
-            //user cancelled login or did not grant authorization
-          }
-        }, {scope:'email'});  	
-      }
+        //user is not connected to your app or logged out
+        set_login_button();
+
+        button.onclick = function() {
+            FB.login(function(response) {
+                if (response.authResponse) {
+                    FB.api('/me', function(response) {
+                        FBOnLoad();
+                    });	   
+                } else {
+                    //user cancelled login or did not grant authorization
+                }
+            }, {scope:'email'});  	
+        }
     }
   }
 
