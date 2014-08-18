c19f22afa376406908c98b137296098fd54d42e4
Prerak Jain <jainprerak@gmail.com>
2014-08-18 23:40:27 +0400

    removed files

--- db/info/views/user_request.js
+++
@@ -1,7 +0,0 @@
-/* Return the requests created by a user */
-
-function(doc) {
-    if(doc.type == "request") {
-        emit( doc.customerId, [doc._id,doc.source,doc.destination,doc.date,doc.time,doc.state] )
-    }
-}
