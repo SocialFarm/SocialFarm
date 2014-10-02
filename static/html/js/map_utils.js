40d14f71bafbebf47d1a4f8f10a14dbdb2386244
Prerak <jainprerak@gmail.com>
2014-10-02 22:50:32 +0530

    map

--- static/html/js/map_utils.js
+++ static/html/js/map_utils.js
@@ -77,7 +77,6 @@ function attachMessage(address,geoLocation,marker,type)
 // function to encode the geolocation to a special format.
 function encodeGeoPosition(lat,lng) {
  
-    //LOG(doc);
     // each deg is 111km ~= 10^2 km
     // 1km = 0.01 deg 
     // 10m = 0.0001 deg
